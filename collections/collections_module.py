9e83add86a10a704b992d778cc8674b8fc347ece
Ana Garrido <anaceciliargp@gmail.com>
2025-12-15 20:36:36 -0300

    Make exercises executable

--- collections/collections_module.py
+++ collections/collections_module.py
@@ -2,15 +2,15 @@
 collections module practice: Counter and defaultdict.
 """
 
-# from collections import Counter
-# frase = input("Digite uma frase: ").lower()
-# c = Counter(frase.split())
-# print(c)
-# print(c.most_common(3))
+from collections import Counter
+frase = input("Digite uma frase: ").lower()
+c = Counter(frase.split())
+print(c)
+print(c.most_common(3))
 
-# from collections import defaultdict
-# grupos = defaultdict(list)
-# frase = input("Digite uma frase: ").lower()
-# for palavra in frase.split():
-#     grupos[len(palavra)].append(palavra)
-# print(dict(grupos))
+from collections import defaultdict
+grupos = defaultdict(list)
+frase = input("Digite uma frase: ").lower()
+for palavra in frase.split():
+    grupos[len(palavra)].append(palavra)
+print(dict(grupos))
