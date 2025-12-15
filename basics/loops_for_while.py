41f55c7f37bdc6297f0d74f4d019d50ed5d32992
Ana Garrido <anaceciliargp@gmail.com>
2025-12-15 20:35:50 -0300

    Make exercises executable

--- basics/loops_for_while.py
+++ basics/loops_for_while.py
@@ -2,19 +2,19 @@
 Loops practice: for/while.
 """
 
-# --- Validação de nota ---
-# nota1 = float(input("Digite a primeira nota: "))
-# while nota1 < 0 or nota1 > 10:
-#     print("Nota inválida!")
-#     nota1 = float(input("Digite outra nota: "))
-# nota2 = float(input("Digite a segunda nota: "))
-# while nota2 < 0 or nota2 > 10:
-#     print("Nota inválida!")
-#     nota2 = float(input("Digite outra nota: "))
-# media = (nota1 + nota2) / 2
-# print(f"MÉDIA = {media:.2f}")
+--- Validação de nota ---
+nota1 = float(input("Digite a primeira nota: "))
+while nota1 < 0 or nota1 > 10:
+    print("Nota inválida!")
+    nota1 = float(input("Digite outra nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+while nota2 < 0 or nota2 > 10:
+    print("Nota inválida!")
+    nota2 = float(input("Digite outra nota: "))
+media = (nota1 + nota2) / 2
+print(f"MÉDIA = {media:.2f}")
 
-# --- Tabuada ---
-# n = int(input("Tabuada de: "))
-# for i in range(1, 11):
-#     print(f"{n} x {i} = {n*i}")
+--- Tabuada ---
+n = int(input("Tabuada de: "))
+for i in range(1, 11):
+    print(f"{n} x {i} = {n*i}")
