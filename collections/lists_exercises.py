bc985ae9bd1d13f1eedc8ea49cf3633d5440798b
Ana Garrido <anaceciliargp@gmail.com>
2025-12-15 20:37:27 -0300

    Make exercises executable

--- collections/lists_exercises.py
+++ collections/lists_exercises.py
@@ -2,20 +2,20 @@
 Lists practice: append, extend, insert, remove, pop, clear, sort, reverse, count, index, split/join.
 """
 
-# lista = [1, 3, 3, 2, 10]
-# lista.append(99)
-# lista.extend([7, 8])
-# lista.insert(1, 42)
-# lista.remove(3)
-# ultimo = lista.pop()
-# print("pop:", ultimo)
-# print("count(3):", lista.count(3))
-# lista.sort()
-# print("sort:", lista)
-# lista.reverse()
-# print("reverse:", lista)
+lista = [1, 3, 3, 2, 10]
+lista.append(99)
+lista.extend([7, 8])
+lista.insert(1, 42)
+lista.remove(3)
+ultimo = lista.pop()
+print("pop:", ultimo)
+print("count(3):", lista.count(3))
+lista.sort()
+print("sort:", lista)
+lista.reverse()
+print("reverse:", lista)
 
-# frase = "ana ama python"
-# palavras = frase.split()
-# print("split:", palavras)
-# print("join:", " ".join(palavras))
+frase = "ana ama python"
+palavras = frase.split()
+print("split:", palavras)
+print("join:", " ".join(palavras))
