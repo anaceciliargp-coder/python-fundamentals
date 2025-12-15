05a62861289be33af84de45fe9abd28e25d3af30
Ana Garrido <anaceciliargp@gmail.com>
2025-12-15 20:37:58 -0300

    Make exercises executable

--- collections/tuples_exercises.py
+++ collections/tuples_exercises.py
@@ -2,7 +2,7 @@
 Tuples practice: creation, indexing, slicing, unpacking, count, index.
 """
 
-# meses = ("January", "February", "March", "April", "May", "June", "July",
-#          "August", "September", "October", "November", "December")
-# x = int(input("Digite um número de 1 a 12: "))
-# print(meses[x-1])
+meses = ("January", "February", "March", "April", "May", "June", "July",
+         "August", "September", "October", "November", "December")
+x = int(input("Digite um número de 1 a 12: "))
+print(meses[x-1])
