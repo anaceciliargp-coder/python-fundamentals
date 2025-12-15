a53c19c56ac25048461a240a529530b508a93be6
Ana Garrido <anaceciliargp@gmail.com>
2025-12-15 20:37:45 -0300

    Make exercises executable

--- collections/sets_exercises.py
+++ collections/sets_exercises.py
@@ -2,5 +2,5 @@
 Sets practice: uniqueness, membership, add/remove/discard, union/intersection/difference.
 """
 
-# frutas = ["banana", "maçã", "banana", "uva", "maçã"]
-# print(set(frutas))
+frutas = ["banana", "maçã", "banana", "uva", "maçã"]
+print(set(frutas))
